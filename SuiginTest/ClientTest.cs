e58fca072366ae26fae0007ddffbecc03c18a0f7
Alfonso J. Ramos <theraot@gmail.com>
2017-05-26 19:30:48 -0500

    Arranged tests alphabetically

--- SuiginTest/ClientTest.cs
+++ SuiginTest/ClientTest.cs
@@ -7,16 +7,16 @@ namespace SuiginTest
     public class ClientTest
     {
         [Fact]
+        public void ConstructorThrowsOnNullString()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Client(connectionString: null));
+        }
+
+        [Fact]
         public void HasConstructorTakingString()
         {
             var client = new Client(connectionString: string.Empty);
             Assert.NotNull(client);
-        }
-
-        [Fact]
-        public void ConstructorThrowsOnNullString()
-        {
-            Assert.Throws<ArgumentNullException>(() => new Client(connectionString: null));
         }
     }
 }
