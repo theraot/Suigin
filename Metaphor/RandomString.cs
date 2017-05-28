f6b591c3db29926f99fe890bab00442f3838652a
Alfonso J. Ramos <theraot@gmail.com>
2017-05-28 06:18:34 -0500

    First Metaphor test
    Alright, now... how do you test randomness with TDD?
    
    I have been searching information on the subject. There are two main
    ideas:
    
    a. Isolate the random generator
    b. Test the distribution of the random generator
    
    ---
    
    I would have to use dependency injection to provide the random number
    generator. Which a good idea, don't get me wrong. Then again, if I
    create an alternative constructor that doesn't take it, it wouldn't be
    covered by tests! - TDD seems to encourage constructors with lots of
    parameters, which seems to encourage "desciptor" types that are only
    used to create other type...
    
    Will see how this evolves.

---
+++ Metaphor/RandomString.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Metaphor
+{
+    public class RandomString
+    {
+        public RandomString(string validCharacters)
+        {
+            GC.KeepAlive(validCharacters);
+        }
+    }
+}
---
+++ MetaphorTest/RandomStringTest.cs
@@ -0,0 +1,15 @@
+﻿using Metaphor;
+using Xunit;
+
+namespace MetaphorTest
+{
+    public class RandomStringTest
+    {
+        [Fact]
+        public void HasConstructorTakingString()
+        {
+            var randomString = new RandomString(validCharacters: string.Empty);
+            Assert.NotNull(randomString);
+        }
+    }
+}
