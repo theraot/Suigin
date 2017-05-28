bc6e5be0d3369aafeeaa9deb4960efb51686b135
Alfonso J. Ramos <theraot@gmail.com>
2017-05-28 09:12:15 -0500

    Actually I need a test with valid input
    If I don't have this, I do not need "if" in the implementation. I could
    just throw ArgumentNullException always.
    
    I should be able to use data driven testing to generate input for this
    case...
    
    Hopefully this can be done simpler...?

--- MetaphorTest/RandomStringTest.cs
+++ MetaphorTest/RandomStringTest.cs
@@ -18,5 +18,13 @@ namespace MetaphorTest
             void RandomCallback(byte[] buffer) { }
             Assert.Throws<ArgumentNullException>(() => new RandomString(randomCallback: RandomCallback, validCharacters: null));
         }
+
+        [Fact]
+        public void ConstructorValidInput()
+        {
+            void RandomCallback(byte[] buffer) { }
+            var randomString = new RandomString(randomCallback: RandomCallback, validCharacters: new char[] { });
+            Assert.NotNull(randomString);
+        }
     }
 }
